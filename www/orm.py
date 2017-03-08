7bd202833cf2932af14e77a9196f33792419529b
zbf <516845590@qq.com>
2017-03-09 00:07:26 +0800

    continue to create the orm

--- www/orm.py
+++ www/orm.py
@@ -4,8 +4,9 @@
 import asyncio,aiomysql
 import logging; logging.basicConfig(level=logging.INFO)
 
-def log(sql,atgs=()):
-    logging.info('SQL %s' % sql)
+#输出方法
+def log(sql,args=()):
+    logging.info('SQL %s,args=%s' % (sql,args))
 
 #创建一个连接池
 @asyncio.coroutine
@@ -59,8 +60,59 @@ def execute(sql,args):
             raise
         return affected
 
+class Field(object):
+    def __init__(self,name,column_type,primary_key,default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s,%s,%s>' %(self.__class__.name,self.column_type,self.name)
+
+#映射varchar
+class StringField(Field):
+    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
+        super().__init__(name,ddl,primary_key,default)
+
+#读取具体子类的映射信息
 class ModelMetaclass(type):
-    pass
+    def __new__(cls, name,bases,attrs):
+        #排除model类本身
+        if name=='Model':
+            return type.__new__(cls,name,bases,attrs)
+        #获取table名称
+        tablename = attrs.get('__table__',None) or name
+        logging.info('found model: %s (table: %s)' %(name,tablename))
+        #获取所有的field的主键名
+        mappings = dict()
+        fields=[]
+        primaryKey = None
+        for k,v in attrs.items():
+            if isinstance(v,Field):
+                logging.info('  found mapping: %s ==> %s' %(k,v))
+                mappings[k] =v
+                if v.primary_key:
+                    #找到主键
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' %k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' %f,fields))
+        attrs['__mappings__'] = mappings        #保留属性和列的映射关系
+        attrs['__table__'] = tablename
+        attrs['__primart_key__'] = primaryKey   #主键属性名
+        attrs['__fields__'] = fields            #出主键外的属性名
+        #构造默认的select,insert,update与delete语句
+        attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey,','.join(escaped_fields),tablename)
+        attrs['__insert__'] = 'insert into `%s` (%s,`%s`) values (%s)' % (tablename,','.join(map(lambda f:'`%s`=?' %(mappings.get(f).name or f),fields)),primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?'%(tablename,primaryKey)
+        return type.__new__(cls,name,bases,attrs)
 
 #ORM映射的基类
 class Model(dict,metaclass=ModelMetaclass):
@@ -89,6 +141,22 @@ class Model(dict,metaclass=ModelMetaclass):
                 setattr(self,key,value)
         return value
 
+    @classmethod
+    @asyncio.coroutine
+    def find(cls,pk):
+        ' find object by primary key. '
+        rs = yield from selcet('%s where `%s`=?' %(cls.__select__,cls.__primary_key__),[pk],1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    @asyncio.coroutine
+    def save(self):
+        args = list(map(self.getValueOrDefault,self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = yield from execute(self.__insert__,args)
+        if rows !=1:
+            logging.warning('failed to insert record:affeoted rows:%s' %rows)
 
 
 
