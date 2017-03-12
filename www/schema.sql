9c7150eceb621580e6c26d5c0fae70635f469ea7
zbf <516845590@qq.com>
2017-03-13 00:20:02 +0800

    finish the sql but it's has some pm

--- www/schema.sql
+++ www/schema.sql
@@ -2,9 +2,48 @@
 
 drop database if exists awesome;
 
-create database awesome
+create database awesome;
 
-use awesome
+use awesome;
+grant select,insert,update,delete on awsome.* to 'www-data'@'localhost' identified by 'www-data';
+
+create table users(
+    `id` varchar(50) not null.
+    `email` varchar(50) not null,
+    `paaawd` varchar(50) not null,
+    `admin` varchar(50) not null,
+    `name` varchar(50) not null,
+    `image` varchar(500) not null,
+    `created_at` real not null,
+    unique key `idx_email` (`email`),
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table blogs(
+    `id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `name` varchar(50) not null,
+    `sumary` varchar(50) not null,
+    `content` varchar(50) not null,
+    `created_at` real not null,
+    key  `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table comments(
+    `id` varchar(50) not null,
+    `blog_id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+)engine=innodb default charset=utf8;
 
 
 
