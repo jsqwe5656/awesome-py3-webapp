e6d3d467a6bb5abfa115f52515ccd7ac2cf4cdd5
zbf <516845590@qq.com>
2017-03-15 12:01:26 +0800

    add configs

---
+++ conf/config_deault.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+configs = {
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'zbf',
+        'password':'123456',
+        'database':'awesome'
+    },
+    'session':{
+        'secret':'AwesOmE'
+    }
+}
